060ebee4c3ca1f1c442c8f625d260f560d5ec38b
Alexandra Paxton <paxton.alexandra@gmail.com>
2023-02-01 17:41:08 -0500

    Create multipanel figure and multipanel table

--- scripts/01-data_analysis-mlam.R
+++ scripts/01-data_analysis-mlam.R
@@ -11,8 +11,20 @@ rm(list=ls())
 library(lme4)
 library(tidyverse)
 library(lmerTest)
-library(devtools)
+library(cowplot)
+library(webshot)
 setwd('~/GitHub/multilevel_linguistic_alignment_across_modalities/')
+
+# thanks to https://rdrr.io/github/kylebutts/kfbmisc/src/R/ggplot_helpers.R
+png_to_grob <- function(source) {
+  
+  temp <- png::readPNG(source)
+  
+  cli::cli_alert_info("Image has width = {dim(temp)[2]}px and height = {dim(temp)[1]}px")
+  
+  temp <- grid::rasterGrob(temp, interpolate = TRUE)
+  return(temp)
+}
 
 ##### 1. Data preparation #####
 
@@ -297,3 +309,176 @@ ggsave(filename = paste0('./figures/plot_conceptual_alignment-mlam.png'),
        height = 6,
        width = 4,
        units = "in")
+
+##### 6. Multi-paneled figure for CogSci 2023 submission #####
+
+# plot: unigram lexical alignment
+plot_subpanel_unigram_lexical_alignment = ggplot(turn_df,
+                                                 aes(y = log_lexical_lem1,
+                                                     x = conv_type,
+                                                     color = conv_type)) +
+  geom_violin() +
+  facet_grid(rows = vars(condition)) +
+  scale_color_manual(values = c("blue", "red","darkgray"),
+                     labels = c("Aff.",
+                                "Arg.",
+                                "Coop."),
+                     name = "Conversation Type")+
+  theme(plot.margin = margin(6, 0, 6, 0))+ 
+  geom_jitter(width=.2,
+              alpha=.1) + 
+  xlab("")  +
+  ylab("Log Alignment") +
+  ggtitle("Lexical unigram")
+
+# plot: unigram syntactic alignment
+plot_subpanel_unigram_syntactic_alignment = ggplot(turn_df,
+                                                   aes(y = syntax_penn_lem1,
+                                                       x = conv_type,
+                                                       color = conv_type)) +
+  geom_violin() +
+  facet_grid(cols = ,
+             rows = vars(condition)) +
+  scale_color_manual(values = c("blue", "red","darkgray"),
+                     labels = c("Aff.",
+                                "Arg.",
+                                "Coop."),
+                     name = "Conversation Type")+
+  geom_jitter(width=.2,
+              alpha=.1) + 
+  theme(plot.margin = margin(6, 0, 6, 0))+ 
+  coord_cartesian(ylim = c(0,1))+
+  xlab("")  +
+  ylab("Syntactic alignment") +
+  ggtitle("Unigram")
+
+# plot: unigram and bigram syntactic alignment
+plot_subpanel_bigram_syntactic_alignment = ggplot(turn_df,
+                                                  aes(y = syntax_penn_lem2,
+                                                      x = conv_type,
+                                                      color = conv_type)) +
+  geom_violin() +
+  facet_grid(rows = vars(condition)) +
+  scale_color_manual(values = c("blue", "red","darkgray"),
+                     labels = c("Aff.",
+                                "Arg.",
+                                "Coop."),
+                     name = "Conv. Type")+
+  theme(plot.margin = margin(6, 0, 6, 0))+ 
+  coord_cartesian(ylim = c(0,1))+
+  geom_jitter(width=.2,
+              alpha=.1) + 
+  xlab("")  +
+  ylab("") +
+  ggtitle("Bigram")
+
+# create subpanels and legend
+panel_unigram_syntactic_alignment = cowplot::plot_grid(
+  plot_subpanel_unigram_syntactic_alignment + theme(legend.position = "none"),
+  labels = c("A")
+)
+panel_bigram_syntactic_alignment = cowplot::plot_grid(
+  plot_subpanel_bigram_syntactic_alignment + theme(legend.position = "none"),
+  labels = c("B")
+)
+plot_alignment_legend = get_legend(plot_subpanel_bigram_syntactic_alignment + 
+                                     theme(legend.position = "bottom", 
+                                           legend.box.margin = margin(0, 0, 0, 0)))
+plot_alignment_data = cowplot::plot_grid(panel_unigram_syntactic_alignment, 
+                                         panel_bigram_syntactic_alignment, 
+                                         nrow=1)
+# create and add title
+plot_alignment_title = ggdraw() + 
+  draw_label("Syntactic alignment by\ncondition and conversation type", 
+             fontface = 'bold', x = 0, hjust = 0) 
+cogsci_fig_2 = cowplot::plot_grid(plot_alignment_title,
+                                  plot_alignment_data,
+                                  # rel_heights = c(0.1, 1, .1),
+                                  rel_heights = c(0.2, 1),
+                                  ncol=1)
+cogsci_fig_2
+
+# save to file
+ggsave(filename = paste0('./figures/cogsci_fig_2-mlam.png'),
+       plot = cogsci_fig_2,
+       height = 4,
+       width = 4,
+       units = "in")
+ 
+##### 7. Multi-paneled table for CogSci 2023 submission #####
+
+# subtable: lexical unigram alignment
+table_subtable_lexical_unigram_alignment = sjPlot::tab_model(model_lexical_unigram_alignment,
+                                                             dv.labels = c("Lexical Unigram Alignment"),
+                                                             pred.labels = c("Intercept",
+                                                                             "Time",
+                                                                             "VC Remote",
+                                                                             "VC Lab",
+                                                                             "Arg.",
+                                                                             "Coop.",
+                                                                             "VC Remote x Arg.",
+                                                                             "VC Lab x Arg.",
+                                                                             "VC Remote x Coop.",
+                                                                             "VC Lab x Arg."),
+                                                             show.re.var = FALSE,
+                                                             show.obs = FALSE,
+                                                             show.icc = FALSE,
+                                                             show.ngroups = FALSE,
+                                                             file = "./tables/table_subtable_lexical_unigram_alignment.html")
+table_subtable_lexical_unigram_alignment
+webshot("./tables/table_subtable_lexical_unigram_alignment.html",
+        "./tables/table_subtable_lexical_unigram_alignment.png",
+        selector = 'table') %>%
+  resize("150%")
+grob_subtable_lexical_unigram_alignment = png_to_grob("./tables/table_subtable_lexical_unigram_alignment.png")
+
+# subtable: lexical bigram alignment
+table_subtable_lexical_bigram_alignment = sjPlot::tab_model(model_lexical_bigram_alignment,
+                                                            dv.labels = c("Lexical Bigram Alignment"),
+                                                            pred.labels = c("Intercept",
+                                                                            "Time",
+                                                                            "VC Remote",
+                                                                            "VC Lab",
+                                                                            "Arg.",
+                                                                            "Coop.",
+                                                                            "VC Remote x Arg.",
+                                                                            "VC Lab x Arg.",
+                                                                            "VC Remote x Coop.",
+                                                                            "VC Lab x Arg."),
+                                                            show.re.var = FALSE,
+                                                            show.icc = FALSE,
+                                                            show.obs = FALSE,
+                                                            show.ngroups = FALSE,
+                                                            file = "./tables/table_subtable_lexical_bigram_alignment.html")
+table_subtable_lexical_bigram_alignment
+webshot("./tables/table_subtable_lexical_bigram_alignment.html",
+        "./tables/table_subtable_lexical_bigram_alignment.png",
+        selector = 'table') %>%
+  resize("150%")
+grob_subtable_lexical_bigram_alignment = png_to_grob("./tables/table_subtable_lexical_bigram_alignment.png")
+
+# bind grobs in cowplot
+table_space = grid::nullGrob()
+table_lexical_alignment_data = cowplot::plot_grid(grob_subtable_lexical_unigram_alignment,
+                                                  table_space,
+                                                  grob_subtable_lexical_bigram_alignment,
+                                                  rel_widths = c(1,.01,1),
+                                                  nrow=1)
+
+# create and add title
+table_alignment_title = ggdraw() + 
+  draw_label("Models predicting lexical alignment", 
+             fontface = 'bold', x = 0, hjust = 0) 
+cogsci_table_1 = cowplot::plot_grid(table_alignment_title,
+                                    table_lexical_alignment_data,
+                                    rel_heights = c(0.1, 1),
+                                    ncol=1)
+cogsci_table_1
+
+# save to file
+ggsave(filename = paste0('./tables/cogsci_table_1-mlam.png'),
+       plot = cogsci_table_1,
+       height = 3,
+       width = 6,
+       units = "in")
+ 
